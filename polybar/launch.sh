cbcf6111163402dcd30d9e2009f825453f553804
rotemad <30571165+rotemad@users.noreply.github.com>
2018-07-08 14:00:49 +0000

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,6 +8,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 sleep 1 && 
-polybar example &
-
+MONITOR=eDP-1 polybar example &	
+MONITOR=DVI-I-1-1 polybar example &
 #echo "Bars launched..."
